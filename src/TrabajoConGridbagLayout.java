609e61866de1d6a1c0aab494711483b4d2a0f048
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-02-10 10:54:06 -0600

    Inicio del programa

--- src/TrabajoConGridbagLayout.java
+++ src/TrabajoConGridbagLayout.java
@@ -1,8 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
+class VentanaInicio extends JFrame{
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
+	
+	public VentanaInicio() {
+		getContentPane().setLayout(gbl);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("GridBagLayaout");
+		setVisible(true);
+		pack();
+		setLocationRelativeTo(null);
+	}
+	public void agregarComponente(JComponent componente, int x, int y, int w, int h) {
+		gbc.gridx = x;
+		gbc.gridy = y;
+		gbc.gridwidth = w;
+		gbc.gridheight = h;
+		gbl.setConstraints(componente, gbc);
+		add(componente);
+	}
+}
 
 public class TrabajoConGridbagLayout {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaInicio();
+			}
+		});
 
 	}
 
