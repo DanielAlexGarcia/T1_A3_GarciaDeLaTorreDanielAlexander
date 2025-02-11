1bd6ddeaee940502bae675bce51c2af2fe4eb19c
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-02-10 18:22:39 -0600

    Se Acomodo de forma basica la interfaz falta 1 cosa por añadir

--- src/TrabajoConGridbagLayout.java
+++ src/TrabajoConGridbagLayout.java
@@ -15,14 +15,38 @@ class VentanaInicio extends JFrame{
 
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 
-		JLabel txt1 = new JLabel("The clasic form includes all visible fields for this list.");
+		JLabel txt1 = new JLabel("The clasic form includes all visible fields for \nthis list.");
 		
 		agregarComponente(txt1, 0, 0, 1, 1);
 
 		JLabel title2 = new JLabel("Form option");
 		agregarComponente(title2, 0, 1, 1, 1);
 
-		JPanel opcionVaciaCorreo = new JPanel();
+		
+
+		JPanel opcionVaciaCorreo = new JPanel(new GridBagLayout());
+			GridBagConstraints gbcPanel = new GridBagConstraints();
+			JLabel panelTxt1 = new JLabel("Email Address");
+			agregarComponente(opcionVaciaCorreo, panelTxt1, 0, 0, 1, 1, gbcPanel);
+
+			JTextArea panelArea1 = new JTextArea();
+			agregarComponente(opcionVaciaCorreo, panelArea1, 0, 1, 1, 1, gbcPanel);
+
+			JLabel panelTxt2 = new JLabel("First Name");
+			agregarComponente(opcionVaciaCorreo, panelTxt2, 0, 2, 1, 1, gbcPanel);
+
+			JTextArea PanelArea2 = new JTextArea();
+			agregarComponente(opcionVaciaCorreo, PanelArea2, 0, 3, 1, 1, gbcPanel);
+
+			JLabel PanelTxt3 = new JLabel("Last Name");
+			agregarComponente(opcionVaciaCorreo, PanelTxt3, 0, 4, 1, 1, gbcPanel);
+
+			JTextArea panelArea3 = new JTextArea();
+			agregarComponente(opcionVaciaCorreo, panelArea3, 0, 5, 1, 1, gbcPanel);
+
+			JButton PanelButon1 = new JButton("Subscribe");
+			agregarComponente(opcionVaciaCorreo, PanelButon1, 0, 6, 1, 1, gbcPanel);
+
 		agregarComponente(opcionVaciaCorreo, 1, 0, 1, 17);
 
 		JLabel txt2 = new JLabel("Include the following");
@@ -55,6 +79,22 @@ class VentanaInicio extends JFrame{
 		JTextArea txtArea2 = new JTextArea();
 		agregarComponente(txtArea2, 0, 11, 1, 1);
 
+		JLabel txt4 = new JLabel("Enhance your form");
+		agregarComponente(txt4, 0, 12, 1, 1);
+
+		JCheckBox check6 = new JCheckBox("enable evil popup mode");
+		agregarComponente(check6, 0, 13, 1, 1);
+
+		JCheckBox check7 = new JCheckBox("disable all JavaScrip");
+		agregarComponente(check7, 0, 14, 1, 1);
+
+		JCheckBox check8 = new JCheckBox("include archive link");
+		agregarComponente(check8, 0, 15, 1, 1);
+
+		JCheckBox check9 = new JCheckBox("include MonkeyRewads link");
+		agregarComponente(check9, 0, 16, 1, 1);
+
+		
 
 
 	}
@@ -65,6 +105,14 @@ class VentanaInicio extends JFrame{
 		gbc.gridheight = h;
 		gbl.setConstraints(componente, gbc);
 		add(componente);
+	}
+	public void agregarComponente(JPanel Panel, JComponent componente, int x, int y, int w, int h, GridBagConstraints gbcc) {
+		gbcc.gridx = x;
+		gbcc.gridy = y;
+		gbcc.gridwidth = w;
+		gbcc.gridheight = h;
+		gbcc.fill = GridBagConstraints.HORIZONTAL;
+		Panel.add(componente, gbcc);
 	}
 }
 
